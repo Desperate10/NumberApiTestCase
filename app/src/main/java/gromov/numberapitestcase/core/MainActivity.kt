8306ea9569fc5f7d709ed9b190393edd7ad12fee
Евгений Громов <gromov.zhenya@gmail.com>
2023-03-12 22:03:59 +0200

    Project setup - gradle build - manifest setting - created MainApplication.kt and MainActivity.kt

--- app/src/main/java/gromov/numberapitestcase/core/MainActivity.kt
+++ app/src/main/java/gromov/numberapitestcase/core/MainActivity.kt
@@ -1,8 +1,11 @@
-package gromov.numberapitestcase
+package gromov.numberapitestcase.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import gromov.numberapitestcase.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

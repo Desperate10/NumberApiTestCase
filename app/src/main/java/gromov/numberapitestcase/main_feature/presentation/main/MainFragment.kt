2caa1eb2c98afebd69e423647d4a9b6e8f183090
Yevhenii Hromov <gromov.zhenya@gmail.com>
2023-03-13 09:58:42 +0200

    Presentation layer start - created navigation graph - created empty fragments - created empty viewModel

---
+++ app/src/main/java/gromov/numberapitestcase/main_feature/presentation/main/MainFragment.kt
@@ -0,0 +1,21 @@
+package gromov.numberapitestcase.main_feature.presentation.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import gromov.numberapitestcase.R
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+}
